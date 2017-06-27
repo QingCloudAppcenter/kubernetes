1f0658b0d6c04a54bbc536e762283b02efb26260
樊尚享 <martinfan@yunify.com>
2017-06-27 19:48:43 +0800

    update fluentd config

--- image/update-cni.sh
+++ image/update-cni.sh
@@ -6,7 +6,7 @@ K8S_HOME=$(dirname "${SCRIPTPATH}")
 source ${K8S_HOME}/version
 
 pushd /tmp
-wget -c https://pek3a.qingstor.com/jolestar/k8s/tools/cni-amd64-${CNI_VERSION}.tgz
+wget -c https://pek3a.qingstor.com/k8s-qingcloud/k8s/tool/cni-amd64-${CNI_VERSION}.tgz
 mkdir -p /opt/cni/bin
 tar -zxvf cni-amd64-${CNI_VERSION}.tgz -C /opt/cni/bin
 rm cni-amd64-${CNI_VERSION}.tgz
@@ -16,6 +16,3 @@ tar -zxvf hostnic.tar.gz -C /opt/cni/bin
 rm hostnic.tar.gz
 
 popd
-
-
-

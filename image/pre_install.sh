b8b7e0ac93a62d8ac9d2bf26a4da9d927099834e
jolestar <jolestar@gmail.com>
2017-06-27 12:34:01 +0800

    update sysctl config

--- image/pre_install.sh
+++ image/pre_install.sh
@@ -13,3 +13,14 @@ rm -rf app-agent-linux-amd64/
 rm app-agent-linux-amd64.tar.gz
 
 apt-get remove network-manager
+
+
+cat << EOF > /etc/sysctl.conf
+net.ipv4.ip_forward = 1
+vm.swappiness = 1
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+
+sysctl -p

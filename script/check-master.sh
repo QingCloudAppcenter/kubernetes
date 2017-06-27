19254de898069f49fcbebd605532163cbf7757c8
jolestar <jolestar@gmail.com>
2017-06-27 19:34:43 +0800

    fix check script

--- script/check-master.sh
+++ script/check-master.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-if systemctl is-active kubelet ;
+if systemctl is-active kubelet
 then
-    if [ $(curl --output /dev/null --silent --fail http://localhost:8080/healthz) == "ok" ]
+    if [ "$(curl --silent --fail http://localhost:8080/healthz)" = "ok" ]
     then
         exit 0
     fi

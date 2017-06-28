e2a4531452caddefeb6b038b2c31fdbaf2c7594d
樊尚享 <martinfan@yunify.com>
2017-06-28 17:39:30 +0800

    move to qingstor to speed up download process

--- image/update-cni.sh
+++ image/update-cni.sh
@@ -11,7 +11,7 @@ mkdir -p /opt/cni/bin
 tar -zxvf cni-amd64-${CNI_VERSION}.tgz -C /opt/cni/bin
 rm cni-amd64-${CNI_VERSION}.tgz
 
-wget -c https://github.com/yunify/hostnic-cni/releases/download/${HOSTNIC_VERSION}/hostnic.tar.gz
+wget -c https://pek3a.qingstor.com/k8s-qingcloud/k8s/tool/hostnic.tar.gz
 tar -zxvf hostnic.tar.gz -C /opt/cni/bin
 rm hostnic.tar.gz
 

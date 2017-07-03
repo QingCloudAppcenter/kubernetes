4969f6050ab25e892b92e01923884e8b2a457711
jolestar <jolestar@gmail.com>
2017-07-03 15:25:42 +0800

    rename registry key

--- sample/qingcloud/registry-secret.sh
+++ sample/qingcloud/registry-secret.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-kubectl create secret docker-registry myregistrykey --docker-username=username --docker-password=password --docker-email=email --docker-server=dockerhub.qingcloud.com
+kubectl create secret docker-registry qingcloud-registrykey --docker-username=username --docker-password=password --docker-email=email --docker-server=dockerhub.qingcloud.com

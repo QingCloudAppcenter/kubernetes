3e6729c3b7fbd699205fc24b04c07ee71e7f2765
Fan Shang Xiang <martinfan@yunify.com>
2017-07-07 14:27:23 +0800

    update kibana to v5.4.0

--- docker/kibana/build.sh
+++ docker/kibana/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker pull gcr.io/google_containers/kibana:v4.6.1-1
-docker tag gcr.io/google_containers/kibana:v4.6.1-1 dockerhub.qingcloud.com/google_containers/kibana:v4.6.1-1
-docker push dockerhub.qingcloud.com/google_containers/kibana:v4.6.1-1
+docker pull gcr.io/google_containers/kibana:v5.4.0
+docker tag gcr.io/google_containers/kibana:v5.4.0 dockerhub.qingcloud.com/google_containers/kibana:v5.4.0
+docker push dockerhub.qingcloud.com/google_containers/kibana:v5.4.0

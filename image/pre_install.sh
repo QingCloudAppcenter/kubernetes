9c2b545f11dd8e22b11e1aee3948a72b80d3f125
jolestar <jolestar@gmail.com>
2017-07-13 13:56:54 +0800

    update hostnic version.

--- image/pre_install.sh
+++ image/pre_install.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-apt-get install -y git wget jq
+apt-get install -y git wget jq apt-transport-https
 
 cd /tmp
 wget https://pek3a.qingstor.com/appcenter/developer/packages/app-agent-linux-amd64.tar.gz
 tar -zxvf app-agent-linux-amd64.tar.gz
 cd app-agent-linux-amd64/
 ./install.sh
+
+chmod +x /etc/init.d/confd
 
 cd /tmp
 rm -rf app-agent-linux-amd64/

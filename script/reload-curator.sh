5cdc755562f95c000a48e9c50e253ea4080c8790
jolestar <jolestar@gmail.com>
2017-07-14 10:20:42 +0800

    enhancement cleanlog job.

--- script/reload-curator.sh
+++ script/reload-curator.sh
@@ -9,7 +9,7 @@ systemctl is-active kubelet >/dev/null 2>&1 && {
     if [ "${HOST_ROLE}" == "master" ]
     then
         JOB_ID=$(timestamp)
-        sed 's/${JOB_ID}/'"${JOB_ID}"'/g' /etc/kubernetes/addons/qingcloud/clean-log-job.yaml /tmp/clean-log-job-${JOB_ID}.yaml
+        sed 's/${JOB_ID}/'"${JOB_ID}"'/g' /etc/kubernetes/addons/qingcloud/clean-log-job.yaml > /tmp/clean-log-job-${JOB_ID}.yaml
         retry mykubectl create -f /tmp/clean-log-job-${JOB_ID}.yaml
         rm /tmp/clean-log-job-${JOB_ID}.yaml
     fi

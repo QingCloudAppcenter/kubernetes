d0275dbdb261c58b0157f6e588fd225e3b0ce1ae
jolestar <jolestar@gmail.com>
2017-07-14 18:01:28 +0800

    remove scale in es.

--- script/scale-in.sh
+++ script/scale-in.sh
@@ -5,11 +5,6 @@ K8S_HOME=$(dirname "${SCRIPTPATH}")
 
 source "${K8S_HOME}/script/common.sh"
 
-if [ "${HOST_ROLE}" == "log" ]
-then
-    scale_es $((LOG_COUNT-1))
-fi
-
 for node in $(cat "/etc/kubernetes/scale_in.info")
 do
     n=$(echo $node|tr '\n' ' ')

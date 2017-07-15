0cec5a2dd5b80c0105b4cef39867300684fc81fb
jolestar <jolestar@gmail.com>
2017-07-15 13:32:28 +0800

    add file-sync docker image.

---
+++ docker/file-sync/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t dockerhub.qingcloud.com/qingcloud/file-sync:0.1 .
---
+++ docker/file-sync/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+function sync_file(){
+    cp -f ${FROM_FILE}  ${TO_FILE}
+    if [ "$?" -ne 0 ]
+    then
+        echo "copy file fail."
+    fi
+}
+
+if [ ! -f ${FROM_FILE} ]
+then
+    echo "file ${FROM_FILE} not exist."
+    exit 1
+fi
+
+LTIME=`stat -c %Z ${FROM_FILE}`
+sync_file
+while true
+do
+   ATIME=`stat -c %Z ${FROM_FILE}`
+
+   if [[ "$ATIME" != "$LTIME" ]]
+   then
+       sync_file
+       LTIME=$ATIME
+   fi
+   sleep 1
+done

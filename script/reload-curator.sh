025857ed29a3d641ade47c777bed2fc22c185bc5
jolestar <jolestar@gmail.com>
2017-07-18 10:02:32 +0800

    clean job log history pod.

--- script/reload-curator.sh
+++ script/reload-curator.sh
@@ -8,6 +8,7 @@ source "${K8S_HOME}/script/common.sh"
 systemctl is-active kubelet >/dev/null 2>&1 && {
     if [ "${HOST_ROLE}" == "master" ]
     then
+        mykubectl delete job -l app=clean-log -n kube-system
         JOB_ID=$(timestamp)
         sed 's/${JOB_ID}/'"${JOB_ID}"'/g' /etc/kubernetes/addons/qingcloud/clean-log-job.yaml > /tmp/clean-log-job-${JOB_ID}.yaml
         retry mykubectl create -f /tmp/clean-log-job-${JOB_ID}.yaml

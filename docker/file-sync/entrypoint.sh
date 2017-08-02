cd416fa78d51ed08cf4bf0bea182c2f0f81694c8
jolestar <jolestar@gmail.com>
2017-08-02 12:08:17 +0800

    update file-sync

--- docker/file-sync/entrypoint.sh
+++ docker/file-sync/entrypoint.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-function sync_file(){
-    d=$(dirname ${TO_FILE})
-    mkdir -p "${d}"
-    cat ${FROM_FILE} > ${TO_FILE}
-    if [ "$?" -ne 0 ]
-    then
-        echo "copy file fail."
-    else
-	echo "${TO_FILE} in sync"
-    fi
-}
+
 
 echo "sync ${FROM_FILE} to ${TO_FILE} "
 while [ ! -f ${FROM_FILE} ]
@@ -22,20 +12,46 @@ done
 # the stat of link will not change since it created.
 if [ -L ${FROM_FILE} ]
 then
-    FROM_FILE=$(readlink -f ${FROM_FILE})
-    echo "from file is symlink, convert to real path: ${FROM_FILE}"
+    REAL_FILE=$(readlink -f ${FROM_FILE})
+    echo "from file is symlink, convert to real path: ${REAL_FILE}"
+else
+    REAL_FILE=${FROM_FILE}
 fi
 
-LTIME=`stat -c %Z ${FROM_FILE}`
-sync_file
+LTIME=`stat -c %Z ${REAL_FILE}`
+sync_file ${REAL_FILE} ${TO_FILE}
 while true
 do
-   ATIME=`stat -c %Z ${FROM_FILE}`
+   ATIME=`stat -c %Z ${REAL_FILE}`
 
    if [[ "$ATIME" != "$LTIME" ]]
    then
-       sync_file
+       sync_file ${REAL_FILE} ${TO_FILE}
        LTIME=$ATIME
    fi
    sleep ${CHECK_INTERVAL:-1}
+   #link target may be change.
+   if [ -L ${FROM_FILE} ]
+   then
+     NEW_REAL_FILE=$(readlink -f ${FROM_FILE})
+     if [ "${NEW_REAL_FILE}" != "${REAL_FILE}" ]
+     then
+        REAL_FILE=${NEW_REAL_FILE}
+        echo "from file symlink is change, convert to real path: ${REAL_FILE}"
+     fi
+   fi
 done
+
+function sync_file(){
+    local F=$1
+    local T=$2
+    local d=$(dirname ${T})
+    mkdir -p "${d}"
+    cat ${F} > ${T}
+    if [ "$?" -ne 0 ]
+    then
+        echo "copy file fail."
+    else
+	echo "${T} in sync with ${F}"
+    fi
+}

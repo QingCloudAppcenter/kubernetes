14daddd04bb7c44b22905b407a1cdb52a50f3f5f
jolestar <jolestar@gmail.com>
2017-08-09 10:54:36 +0800

    update docker images.

--- docker/elasticsearch/build.sh
+++ docker/elasticsearch/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
- docker pull gcr.io/google_containers/elasticsearch:v5.4.0-1
- docker tag gcr.io/google_containers/elasticsearch:v5.4.0-1 dockerhub.qingcloud.com/google_containers/elasticsearch:v5.4.0-1
- docker push dockerhub.qingcloud.com/google_containers/elasticsearch:v5.4.0-1
+docker pull gcr.io/google_containers/elasticsearch:v5.5.1
+docker tag gcr.io/google_containers/elasticsearch:v5.5.1 dockerhub.qingcloud.com/google_containers/elasticsearch:v5.5.1
+docker push dockerhub.qingcloud.com/google_containers/elasticsearch:v5.5.1

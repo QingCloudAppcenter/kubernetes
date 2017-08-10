81cc4184a0424a9a6a5e207f80694fd37f7de5e4
jolestar <jolestar@gmail.com>
2017-08-10 16:48:38 +0800

    update init client.

--- script/init-client.sh
+++ script/init-client.sh
@@ -4,6 +4,7 @@ K8S_HOME=$(dirname "${SCRIPTPATH}")
 
 source "${K8S_HOME}/script/common.sh"
 
+echo "root:k8s" |chpasswd
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart ssh
 

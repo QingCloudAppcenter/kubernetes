550f51dc594ff739cb95779f920fc4694f95de57
jolestar <jolestar@gmail.com>
2017-08-11 12:28:29 +0800

    update install script.

--- image/install-pkg.sh
+++ image/install-pkg.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 apt-get update
+
+apt-get install -y ebtables socat
+apt-get install -y jq apt-transport-https bash-completion
+
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 apt-get update
 apt-cache policy docker-engine
 apt-get install -y docker-engine
 
-apt-get install -y ebtables socat
-apt-get install -y jq apt-transport-https bash-completion
 apt-get remove network-manager
 
 apt-get upgrade -y

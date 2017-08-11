4feb9bc897c8f7a1d8215f6954eb3ca0d29cfd8d
jolestar <jolestar@gmail.com>
2017-08-11 18:04:33 +0800

    update kubeconfig confd

--- image/install-qingcloud-agent.sh
+++ image/install-qingcloud-agent.sh
@@ -12,5 +12,11 @@ cd /tmp
 rm -rf app-agent-linux-amd64/
 rm app-agent-linux-amd64.tar.gz
 
+echo "upgrade confd"
+wget https://github.com/yunify/confd/releases/download/v0.13.10/confd-linux-amd64.tar.gz
+tar -O -zxf confd-linux-amd64.tar.gz >/opt/qingcloud/app-agent/bin/confd
+chmod +x /opt/qingcloud/app-agent/bin/confd
+rm confd-linux-amd64.tar.gz
+
 systemctl enable confd
 systemctl disable confd

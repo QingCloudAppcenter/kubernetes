9017c08e191677e4900c26e816e587110a291820
jolestar <jolestar@gmail.com>
2017-08-11 12:03:34 +0800

    add adminconf to metadata

--- script/get-metadata.sh
+++ script/get-metadata.sh
@@ -7,4 +7,5 @@ source "${K8S_HOME}/script/common.sh"
 ensure_dir
 
 init_token=$(get_or_gen_init_token)
-echo '{"init_token":"'${init_token}'"}'
+adminconf=$(cat "/etc/kubernetes/admin.conf")
+echo '{"init_token":"'${init_token}'", "adminconf":'${adminconf}'}'

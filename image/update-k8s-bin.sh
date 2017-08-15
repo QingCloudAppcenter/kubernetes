b3f66bd2a90c4815b4592e373497887f792f1a72
jolestar <jolestar@gmail.com>
2017-08-15 17:31:32 +0800

    add qingcloud-volume-provisioner config

--- image/update-k8s-bin.sh
+++ image/update-k8s-bin.sh
@@ -18,12 +18,19 @@ function download_k8s_bin()
     for bin in "${k8s_bins[@]}"; do
         local bin_url="$k8s_base_url/${bin}"
         echo "downloading ${bin_url}"
-        wget -c ${bin_url}
-        unlink "${k8s_bin_path}/${bin}"
-        ln -s "${K8S_HOME}/bin/${bin}"  "${k8s_bin_path}/${bin}"
+        wget ${bin_url}
+        ln -fs "${K8S_HOME}/bin/${bin}"  "${k8s_bin_path}/${bin}"
     done
+    wget "https://pek3a.qingstor.com/k8s-qingcloud/k8s/qingcloud/volume/qingcloud-flex-volume"
     chmod +x *
+    ln -fs "${K8S_HOME}/bin/qingcloud-flex-volume"  "${k8s_bin_path}/qingcloud-flex-volume"
+    "${K8S_HOME}/bin/qingcloud-flex-volume" --install=true
     popd
+}
+
+function download_flex_volume_driver()
+{
+    /usr/libexec/kubernetes/kubelet-plugins/volume/exec/
 }
 
 rm -rf ${K8S_HOME}/bin/*

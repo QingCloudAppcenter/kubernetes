9c0ebe2ae72b8094117656d98a492df76c6b8f39
jolestar <jolestar@gmail.com>
2017-08-16 18:28:23 +0800

    remove client image script.

--- client_image/script/reload_interface.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ifdown eth0 && ifup eth0
--- script/init-client.sh
+++ script/init-client.sh
@@ -7,6 +7,3 @@ source "${K8S_HOME}/script/common.sh"
 echo "root:k8s" |chpasswd
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart ssh
-
-ensure_dir
-join_node

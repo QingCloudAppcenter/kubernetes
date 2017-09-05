2a658520e4401446ac88a603aaa8336f5184dca4
樊尚享 <martinfan@yunify.com>
2017-09-05 12:47:58 +0800

    add logrotate conf for flex plugin

--- image/update-logrotate.sh
+++ image/update-logrotate.sh
@@ -5,6 +5,6 @@ K8S_HOME=$(dirname "${SCRIPTPATH}")
 
 echo "update logrotate"
 
-cp -f ${K8S_HOME}/logrotate/conf /etc/logrotate.d/containerslog
 cp -f ${K8S_HOME}/logrotate/docker-containers /etc/logrotate.d/docker-containers
 cp -f ${K8S_HOME}/logrotate/cron /etc/cron.hourly/logrotate
+cp -f ${K8S_HOME}/logrotate/flex-volume  /etc/logrotate.d/flex-volume

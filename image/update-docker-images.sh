b63ec10e44026d7ca845cb4f33bdfbf75b7b72be
樊尚享 <martinfan@yunify.com>
2017-09-07 11:47:39 +0800

    update cni plugin version

--- image/update-docker-images.sh
+++ image/update-docker-images.sh
@@ -36,4 +36,4 @@ docker pull dockerhub.qingcloud.com/qingcloud/file-sync:0.7
 docker pull dockerhub.qingcloud.com/busybox:1.27.1
 docker pull dockerhub.qingcloud.com/qingcloud/qingcloud-volume-provisioner:v1.0
 docker pull dockerhub.qingcloud.com/qingcloud/qingcloud-cloud-controller-manager:v1.0
-docker pull dockerhub.qingcloud.com/qingcloud/hostnic-cni:v0.8.1
+docker pull dockerhub.qingcloud.com/qingcloud/hostnic-cni:v0.8.2

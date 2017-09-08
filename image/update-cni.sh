1a7b8576491ad44f2e07fa6bd291bbbd5f847c92
樊尚享 <martinfan@yunify.com>
2017-09-08 10:56:49 +0800

    update hostnic version

--- image/update-cni.sh
+++ image/update-cni.sh
@@ -6,7 +6,7 @@ K8S_HOME=$(dirname "${SCRIPTPATH}")
 source ${K8S_HOME}/version
 
 CNI_VERSION=v0.5.2
-HOSTNIC_VERSION=v0.8.2
+HOSTNIC_VERSION=v0.8.3
 
 pushd /tmp
 wget -c https://pek3a.qingstor.com/k8s-qingcloud/k8s/tool/cni-amd64-${CNI_VERSION}.tgz

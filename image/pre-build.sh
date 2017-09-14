5e01fd14a889dbf36153b1738ec5f4edc6ce9572
Calvin Yu <calvinyu@yunify.com>
2017-09-14 16:46:18 +0800

    enhance pre-build script

--- image/pre-build.sh
+++ image/pre-build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-
+gitBranch=$1
 apt-get install -y git
 git clone https://github.com/QingCloudAppcenter/kubernetes.git /opt/kubernetes
-cd /opt/kubernetes/image
-
-build.sh
-
+cd /opt/kubernetes
+if [ -n "$gitBranch" ]
+then
+  git fetch origin $gitBranch:$gitBranch
+  git checkout $gitBranch
+  git branch --set-upstream-to=origin/$gitBranch $gitBranch
+  git pull
+fi
+/opt/kubernetes/image/build.sh

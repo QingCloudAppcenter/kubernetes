ec5a6d78e6577d668991c70d67e7b686a547177b
樊尚享 <martinfan@yunify.com>
2017-09-15 22:03:26 +0800

    update fluentbit version

--- docker/fluentbit/build.sh
+++ docker/fluentbit/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker pull fluent/fluent-bit:0.12
-docker tag fluent/fluent-bit:0.12 dockerhub.qingcloud.com/fluent/fluent-bit:0.12
-docker push dockerhub.qingcloud.com/fluent/fluent-bit:0.12
+docker pull fluent/fluent-bit:0.12.2
+docker tag fluent/fluent-bit:0.12.2 dockerhub.qingcloud.com/fluent/fluent-bit:0.12.2
+docker push dockerhub.qingcloud.com/fluent/fluent-bit:0.12.2

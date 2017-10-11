0959fa4976a28b1620a262b5e97090dd178f49cd
Calvin Yu <calvinyu@yunify.com>
2017-10-11 13:11:32 +0800

    address jole's comment

--- script/upgrade-node.sh
+++ script/upgrade-node.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-version=$1
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 K8S_HOME=$(dirname "${SCRIPTPATH}")
 
 source "${K8S_HOME}/script/common.sh"
 
 ensure_dir
-if [ -n "$version" ]
-then
-  link_dynamic_dir
-  docker_login
-else
-  upgrade_docker
-fi
+link_dynamic_dir
+docker_login

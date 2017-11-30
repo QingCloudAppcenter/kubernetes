cf4fc297094da486fad1719bb1f5d4d6bac80c7a
Calvin Yu <calvinyu@yunify.com>
2017-11-30 16:46:08 +0800

    support k8s 1.8.4

--- docker/fluentbit/build.sh
+++ docker/fluentbit/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker pull fluent/fluent-bit:0.12.2
-docker tag fluent/fluent-bit:0.12.2 dockerhub.qingcloud.com/fluent/fluent-bit:0.12.2
-docker push dockerhub.qingcloud.com/fluent/fluent-bit:0.12.2
+docker pull fluent/fluent-bit:0.12.9
+docker tag fluent/fluent-bit:0.12.9 dockerhub.qingcloud.com/fluent/fluent-bit:0.12.9
+docker push dockerhub.qingcloud.com/fluent/fluent-bit:0.12.9

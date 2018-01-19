9856b6f61f004d1141072109e0a9f1742808ff4d
Calvin Yu <calvinyu@yunify.com>
2018-01-19 18:19:11 +0800

    integreate some fixes

--- image/update-pkg.sh
+++ image/update-pkg.sh
@@ -2,15 +2,17 @@
 
 apt-get update
 
-apt-get install -y ebtables socat jq apt-transport-https bash-completion ntp wget
+apt-get install -y ebtables socat jq apt-transport-https bash-completion ntp wget ca-certificates curl oftware-properties-common
 
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 apt-get update
 
-apt-cache policy docker-engine
-apt-get install -y docker-engine
+apt-get install -y docker-ce
 
 apt-get remove network-manager
 

71a0f15917cdff67a4b7785f375cd7e698099472
Calvin Yu <calvinyu@yunify.com>
2018-06-11 17:13:35 +0800

    add ipvs support

--- image/update-pkg.sh
+++ image/update-pkg.sh
@@ -19,4 +19,6 @@ apt-get remove -y network-manager
 
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
+modprobe ip_vs ip_vs_rr ip_vs_wrr ip_vs_sh nf_conntrack_ipv4
+
 
